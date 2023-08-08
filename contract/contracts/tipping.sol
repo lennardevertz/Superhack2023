36395bd4dca141250b0e02953e6f53773cce09de
lennardevertz <levertz@idriss.xyz>
2023-08-08 21:58:34 +0200

    add the usual dependencies and boilerplate contract struct

---
+++ contract/contracts/tipping.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+
+/**
+ * @title PlayPal 
+ * @author Lennard (lennardevertz)
+ * @notice Tipping contract to support tips on twitch (and other platforms in the future)
+ */
+contract PlayPal {
+    constructor(){
+    }
+}
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,4 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@truffle/dashboard-hardhat-plugin");
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.17",
+  networks: {
+    truffledashboard: {
+      url: "http://localhost:24012/rpc"
+    },
+  },
 };
